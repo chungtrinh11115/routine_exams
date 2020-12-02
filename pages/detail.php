eff79bbee353e3290d20405ece2fab7fa8e70f19
Ann95-gif <c190327@ccg.ac.jp>
2020-12-02 00:43:22 +0000

    20201202

--- pages/detail.php
+++ pages/detail.php
@@ -7,20 +7,29 @@
     
     try {
     	$pdo = new PDO($dsn, $user, $password);
-    	$sql = "select * from restaurants where id=?";
-    	$pstmt = $pdo->prepare($sql);
-    	$pstmt->bindValue(1, $id);
-    	$pstmt->execute();
-    	$records = [];
-    	$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
-    	unset($pstmt);
-    	unset($pdo);
+        // 実行するSQLを設定
+        $sql = "select * from restaurants where id = ?";
+        $sqlRv = "select * from reviews where restaurant_id = ?";
+        // SQL実行オブジェクトを取得
+        $pstmt = $pdo->prepare($sql);
+        $pstmtRv = $pdo->prepare($sqlRv);
+        // プレースホルダにリクエストパラメータを設定
+        $pstmt->bindValue(1, $id);
+        $pstmtRv->bindValue(1, $id);
+        // SQLを実行
+        $pstmt->execute();
+        $pstmtRv->execute();
+        // SQL実行結果を配列に取得
+        $records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+        $recordsRv = $pstmtRv->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $e) { 
     	echo $e->getMessage();
     } 
 
     
+    var_dump($id);
     var_dump($records);
+    var_dump($recordsRv);
 ?>
 
 <!DOCTYPE html>
@@ -34,63 +43,57 @@
 <body id="detail">
 	<div class="p-wrapper">
 	<header>
-		<h1><a href="list.html">レストラン レビュ サイト</a></h1>
+		<h1><a href="list.php">レストラン レビュ サイト</a></h1>
 	</header>
 	<main>
 		<article class="detail">
 			<h2>レストラン詳細</h2>
 			<section>
 				<table class="list">
-					<tr>
-					    <?php if(count($records) > 0): ?>
-					        <?php foreach ($records as $record): ?>
-        						<td class="photo"><img name="image" src="../pages/img/<?= $record[image] ?> "/></td>
-        						<td class="info">
-        							<dl>
-        								<dt name="name"><?= $record[name] ?></dt>
-        								<dd name="description">
-        									<?= $record[description] ?>
-        								</dd>
-        							</dl>
-        						</td>
-    						<?php endforeach;?>
-						<?php endif;?>
-					</tr>
+				    <?php foreach ($records as $record): ?>
+    					<tr>
+    						<td class="photo"><img name="image" src="../pages/img/<?= $record["image"] ?>" /></td>
+    						<td class="info">
+    							<dl>
+    								<dt name="name"><?= $record["name"] ?></dt>
+    								<dd name="description">
+    									<?= $record["description"] ?>。
+    								</dd>
+    							</dl>
+    						</td>
+    					</tr>
+					<?php endforeach; ?>
 				</table>
 			</section>
 		</article>
 		<article class="reviews">
 			<h2>レビュ一覧</h2>
+			<?php if (count($recordsRv) > 0): ?>
 			<section>
-				<dl class="review">
-					<dt name="point">★★★★☆</dt>
-					<dd name="description">
-							常連の者で、いつも夫婦で伺っています。
-							席数が少ないので予約した方が安心ですが、その分落ち着いて食事できますよ。
-							コースのメインは基本的にシェフにお任せ。
-							来るたびに、新しい味との出会いを楽しめるお店です。
-							<div name="posted">
-								（<span name="posted_at">2020-09-14 05:29:01</span><span name="reviewer">totsuka</span>さん）
-							</div>
-					</dd>
-				</dl>
-				<dl class="review">
-					<dt name="point">★★★★★</dt>
-					<dd name="description">
-							説明の通り、喧騒を外れた場所にひっそりとあるレストランでした。
-							伊豆市には初めて来ましたが、本当に桜がきれいですね。
-							何よりも空気がきれいで、いいリフレッシュになりました。
-							<div name="posted">
-								（<span name="posted_at">2020-09-12 15:43:45</span><span name="reviewer">oie</span>さん）
-							</div>
-					</dd>
-				</dl>
+			    <?php foreach ($recordsRv as $recordRv): ?>
+    				<dl class="review">
+    					<dt name="point" id="point">
+    					    <?php
+					        $point = intval($recordRv["point"]);
+					        $str = str_repeat("★", $point).str_repeat("☆", 5 - $point);
+					        echo $str;
+                            ?>
+    					</dt>
+    					<dd name="description">
+    							<?= $recordRv["comment"] ?>
+    							<div name="posted">
+    								（<span name="posted_at"><?= $recordRv["posted_at"] ?></span><span name="reviewer"><?= $recordRv["reviewer"] ?></span>さん）
+    							</div>
+    					</dd>
+    				</dl>
+				<?php endforeach; ?>
 			</section>
+			<?php endif; ?>
 		</article>
 		<article class="entry">
 			<h2>レビュを書き込む</h2>
 			<section>
-				<form action="detail.html" method="post">
+				<form action="detail.php" method="post">
 					<table class="entry">
 						<tr>
 							<th>お名前</th>
@@ -121,6 +124,7 @@
 					</div>
 				</form>
 			</section>
+			
 		</article>
 	</main>
 	<footer>
